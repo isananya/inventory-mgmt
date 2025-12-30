7ef65f597ddd8283caa580105c53f3c8d7972e78
isananya <nayakananya2004@gmail.com>
2025-12-30 17:16:09 +0530

    feat (user service) : add models

---
+++ user-service/src/main/java/com/chubb/inventoryapp/model/Role.java
@@ -0,0 +1,9 @@
+package com.chubb.inventoryapp.model;
+
+public enum Role {
+	ADMIN,
+	SALES_EXECUTIVE,
+	WAREHOUSE_MANAGER,
+	FINANCE_OFFICER,
+	CUSTOMER
+}
---
+++ user-service/src/main/java/com/chubb/inventoryapp/model/User.java
@@ -0,0 +1,44 @@
+package com.chubb.inventoryapp.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Email(message = "Invalid email format")
+    @NotBlank(message = "Email is required")
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @NotBlank
+    @Size(min = 8, message = "Password must be at least 8 characters")
+    private String password;
+
+    @NotBlank
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @NotNull
+    private Role role;
+
+    private boolean active = true;
+}
+
