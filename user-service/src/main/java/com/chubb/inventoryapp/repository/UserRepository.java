ed47b9038a73eca49d4231290e07de8e8465c2a1
isananya <nayakananya2004@gmail.com>
2025-12-30 17:39:34 +0530

    feat (user service) : add repository

---
+++ user-service/src/main/java/com/chubb/inventoryapp/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.chubb.inventoryapp.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.chubb.inventoryapp.model.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	  Optional<User> findByEmail(String email);
+	  
+	  boolean existsByEmail(String email);
+	}
