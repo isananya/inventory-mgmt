eb3ec734c014fe690b09e8e3971cb95f2b3e139a
isananya <nayakananya2004@gmail.com>
2026-01-04 16:35:21 +0530

    fix : resolve test issues

--- backend/billing-service/src/test/java/com/chubb/inventoryapp/BillingServiceApplicationTests.java
+++ backend/billing-service/src/test/java/com/chubb/inventoryapp/BillingServiceApplicationTests.java
@@ -3,7 +3,7 @@ package com.chubb.inventoryapp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
+@SpringBootTest
 class BillingServiceApplicationTests {
 
 	@Test
